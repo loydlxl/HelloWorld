402e86c40fb8afa5b25d41adddd87af6d499840a
loydlxl <moses.taruc@gmail.com>
2013-11-28 22:02:40 +0800

    initial commit

---
+++ src/com/loyd/practice/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.loyd.practice;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+ 
+}
